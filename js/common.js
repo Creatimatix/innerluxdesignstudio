35a283bbe473a667606dd41e3eee55484e0aa065
Sonu Verma <sonu.verma0715@gmail.com>
2024-12-17 16:21:00 +0530

    project setup

---
+++ js/common.js
@@ -0,0 +1,59 @@
+document.getElementById('navbar_html').innerHTML = `
+<div class="pbmit-main-header-area">
+				<div class="container-fluid">
+					<div class="pbmit-header-content d-flex justify-content-between align-items-center">
+						<div class="site-branding">
+							<h1 class="site-title">
+								<a href="index.html">
+									<img class="logo-img" src="images/logo.png" alt="Innerlux Design Studio" style="max-height: 83px !important;">
+								</a>
+							</h1>
+						</div>
+						<div class="pbmit-menuarea">
+							<div class="site-navigation">
+								<nav class="main-menu navbar-expand-xl navbar-light">
+									<div class="navbar-header">
+										<!-- Toggle Button --> 
+										<button class="navbar-toggler" type="button">
+											<i class="pbmit-base-icon-menu-1"></i>
+										</button>
+									</div>
+									<div class="pbmit-mobile-menu-bg"></div>
+									<div class="collapse navbar-collapse clearfix show" id="pbmit-menu">
+										<div class="pbmit-menu-wrap">
+											<span class="closepanel">
+												<svg class="qodef-svg--close qodef-m" xmlns="http://www.w3.org/2000/svg" width="20.163" height="20.163" viewBox="0 0 26.163 26.163">
+													<rect width="36" height="1" transform="translate(0.707) rotate(45)"></rect>
+													<rect width="36" height="1" transform="translate(0 25.456) rotate(-45)"></rect>
+												</svg>
+											</span>
+											<ul class="navigation clearfix">
+												<li class="dropdown active">
+													<a href="index.html">Home</a>
+												</li>
+												<li class="dropdown">
+													<a href="#">Pages</a>
+													<ul>
+														<li><a href="our-history.html">Our History</a></li>
+														<li><a href="our-team.html">Our Team</a></li>
+														<li><a href="team-single-details.html">Team Single Details</a></li>
+														<li><a href="faq.html">Faq</a></li>
+													</ul>
+												</li>
+												<li class="dropdown">
+													<a href="#">Services</a>
+													<ul>
+														<li><a href="service-details.html">Service Detail</a></li>
+													</ul>
+												</li>
+												<li><a href="about-us.html">About Us</a></li>
+												<li><a href="contact-us.html">Contact Us</a></li>
+											</ul>
+										</div>
+									</div>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>`;
